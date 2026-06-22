9e1fb742eb2885d2b1dcec7fa9b94176c1f44662
Mayra Aguillar <mayra.aguilar3001@gmail.com>
2026-06-22 17:04:08 -0600

    Base de datos creada correctamente

--- EcoSystem.Data/obj/Debug/net10.0/EcoSystem.Data.AssemblyInfo.cs
+++ EcoSystem.Data/obj/Debug/net10.0/EcoSystem.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d42a9377676fbd7dae5aad1e3575bb530512afb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46121af7c8eba1f9ee3b55864ac51610214359db")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
