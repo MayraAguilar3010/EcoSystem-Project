c46b6514976ea42655e30debe013656560852352
Mayra Aguilar <mayra.aguilar3001@gmail.com>
2026-08-02 21:18:43 -0600

    fix: use console logging for local hosting

--- EcoSystem.API/Program.cs
+++ EcoSystem.API/Program.cs
@@ -11,6 +11,9 @@ using System.Text;
 Environment.SetEnvironmentVariable("DOTNET_hostBuilder__reloadConfigOnChange", "false");
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
@@ -154,4 +157,5 @@ static async Task AddUserIfConfiguredAsync(
     context.Users.Add(user);
     await context.SaveChangesAsync();
 }
+
 
